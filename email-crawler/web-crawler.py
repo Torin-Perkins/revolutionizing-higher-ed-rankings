32eb1488166fa6bb6e0d50a9d57fe4bf59316867
Torin-Perkins <tdmp42@gmail.com>
2023-11-14 15:42:08 -0800

    Web crawler working
    - Only based on the data given by cs-rankings
    - Next goal is to collect data a-z using given csv files
    - Goal is also to dynamically collect data

--- email-crawler/web-crawler.py
+++ email-crawler/web-crawler.py
@@ -1,3 +1,5 @@
+import time
+
 from bs4 import BeautifulSoup
 import requests, json
 import sys
@@ -13,19 +15,39 @@ Notes 11/4/2023
 Check CSRanking ./util because contains good information about getting records from dblp
 May want to add something about cross referencing institutions with countries to ensure we stay in the US
 """
+def filter_america(author_list):
+    us_schools = csv.reader(open("us_universities.csv", "r"), delimiter=",")
+    us_schools_list = []
+    updated_author_list = []
+    for row in us_schools:
+        us_schools_list.append(row[0])
+
+    for item in author_list:
+        if item[1] in us_schools_list:
+            updated_author_list.append(item)
+    return updated_author_list
 
 
 def run_scraper():
     file_name = 'csrankings-a'
     output_file = file_name + '_emails.csv'
-    csv_file = csv.reader(open(file_name + '.csv', "r"), delimiter=",")
+    csv_file = csv.reader(open(file_name + '.csv', "r", encoding="utf-8"), delimiter=",")
     csv.writer(open(output_file, "w")).writerow(['site title', 'email found'])
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
-
+    author_list = []
     for row in csv_file:
-        search_term = row[0] + ' ' + row[1]
+        #print(row[0])
+        #print(row[1])
+        author_list.append([row[0], row[1]])
+
+    updated_author_list = filter_america(author_list)
+    #time.sleep(5)
+    print(len(author_list))
+    print(len(updated_author_list))
+    for item in updated_author_list:
+        search_term = item[0] + ' ' + item[1]
         search_term = re.sub(r"[^\w\s]", '', search_term)
         search_term = re.sub(r"\s+", '+', search_term)
         print(search_term)
@@ -70,9 +92,9 @@ def run_scraper():
                 continue
         print(Data)
 
-        for item in Data:
+        for item1 in Data:
             try:
-                response = requests.get(item['link'])
+                response = requests.get(item1['link'])
             except:
                 print("Some error occurred")
             if response.status_code == 200:
@@ -87,7 +109,7 @@ def run_scraper():
                         if 'mailto' in str(i):  # if mailto is seen anywhere in the link
                             print(i['href'][7:])  # Cuts out the 'mailto:' part
                             file = open(output_file, "a")
-                            csv.writer(file).writerow([item['title'], i['href'][7:]])
+                            csv.writer(file).writerow([item[0], i['href'][7:]])
                             file.close()
 
                             break  # Only one email from each page
@@ -95,8 +117,8 @@ def run_scraper():
                 else:
                     print("No mailto links found on the webpage.")
             else:
-                print(f"Failed to fetch content from {item['link']}")
+                print(f"Failed to fetch content from {item1['link']}")
 
 
-#run_scraper()
-parse_dblp()
+run_scraper()
+
